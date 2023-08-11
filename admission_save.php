2bcaa449c5c1f622c8482303768b1fd658608779
sabbir hossain <mdsabbir477470@gmail.com>
2023-08-11 23:16:39 +0600

    admission project set up

---
+++ admission_save.php
@@ -0,0 +1,24 @@
+<?php
+
+echo $_POST['email'];
+echo'<br>';
+echo $_POST['uname'];
+echo'<br>';
+echo $_POST['pwd'];
+echo'<br>';
+echo $_POST['cpwd'];
+echo'<br>';
+echo $_POST['fname'];
+echo'<br>';
+echo $_POST['phno'];
+echo'<br>';
+echo $_POST['phno_2'];
+// echo'<br>';
+// echo $_POST[file_get_contents($_FILES['pic'])];
+// echo'<br>';
+// echo $_POST['pic2'];
+
+
+
+
+?>
